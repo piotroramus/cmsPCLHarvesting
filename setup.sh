d3b5e471c11150feb735a7162bae9a62fc98db8a
Piotr Oramus <piotr.karol.oramus@cern.ch>
2016-04-14 16:44:53 +0200

    Checking if script was sourced

--- setup.sh
+++ setup.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
-# TODO: is there any way to check if script was sourced or if it was run?
-#       basically this script should be sourced
+# magic to check if the script was sourced
+if [[ ! ${0##*/} == -* ]]; then
 
-echo "Generating new proxy certificate..."
-voms-proxy-init -rfc -voms cms
-export X509_USER_PROXY=$(voms-proxy-info --path)
+    echo "This script should be sourced rather than executed in different shell"
+
+else
+
+    echo "Generating new proxy certificate..."
+    voms-proxy-init -rfc -voms cms
+    export X509_USER_PROXY=$(voms-proxy-info --path)
 
 
-# TODO: first check if user has an access to /cvmfs/cms.cern.ch - if not raise an error message
-source /cvmfs/cms.cern.ch/crab3/crab.sh
+    # TODO: first check if user has an access to /cvmfs/cms.cern.ch - if not raise an error message
+
+    source /cvmfs/cms.cern.ch/crab3/crab.sh
+fi
