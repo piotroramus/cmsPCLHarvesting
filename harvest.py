04138c19f5115dcf594abf16fe9c7e8268de211a
Piotr Oramus <piotr.karol.oramus@cern.ch>
2016-04-18 12:48:09 +0200

    SQLAlchemy model decoupled from harvesting script

--- harvest.py
+++ harvest.py
@@ -2,72 +2,18 @@ import datetime
 
 from dbs.apis.dbsClient import DbsApi
 
-from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean, ForeignKey
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker
 
 import logging
 from rrapi import RRApi, RRApiError
 from logs.logger import setup_logging
 
+from model import Base, RunInfo, RunBlock, Multirun, Filename
+
+
 setup_logging()
 logger = logging.getLogger(__name__)
-
-Base = declarative_base()
-
-
-# TODO: decouple model from this script
-class RunInfo(Base):
-    __tablename__ = 'run_info'
-
-    number = Column(Integer, primary_key=True)
-    run_class_name = Column(String)
-    bfield = Column(Float)
-    start_time = Column(DateTime)
-    stop_time = Column(DateTime)
-    # TODO: probably no need for LS
-    ls_count = Column(Integer)
-
-    run_blocks = relationship("RunBlock")
-    multirun = Column(Integer, ForeignKey('multirun.id'))
-
-    def __repr__(self):
-        return "RunInfo(number={}, run_class_name={}, bfield={} start_time={}, end_time={}, ls_count={})".format(
-            self.number, self.run_class_name, self.bfield, self.start_time, self.stop_time, self.ls_count)
-
-
-class RunBlock(Base):
-    __tablename__ = 'run_block'
-
-    id = Column(Integer, primary_key=True)
-    block_name = Column(String)
-
-    run_number = Column(Integer, ForeignKey('run_info.number'))
-
-
-class Multirun(Base):
-    __tablename__ = 'multirun'
-
-    id = Column(Integer, primary_key=True)
-    number_of_events = Column(Integer)
-    dataset = Column(String)
-    bfield = Column(Float)
-    run_class_name = Column(String)
-    closed = Column(Boolean)
-
-    run_numbers = relationship("RunInfo")
-    filenames = relationship("Filename")
-
-
-class Filename(Base):
-    __tablename__ = 'filename'
-
-    id = Column(Integer, primary_key=True)
-    filename = Column(String)
-
-    multirun = Column(Integer, ForeignKey('multirun.id'))
-
 
 url = "https://cmsweb.cern.ch/dbs/prod/global/DBSReader"
 dbsApi = DbsApi(url=url)
