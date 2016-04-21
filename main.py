6589060b6cabff90094bb5f6343d229c7933449b
Piotr Oramus <piotr.karol.oramus@cern.ch>
2016-04-21 12:35:00 +0200

    Added logging to main module

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import logging
+
 from dataDiscovery.harvest import harvest
+from logs.logger import setup_logging
+
 
 if __name__ == '__main__':
+
+    setup_logging()
+    logger = logging.getLogger(__name__)
+
     # TODO #3: consider calling setup.sh here
+    logger.info("Starting data discovery")
     harvest()
+    logger.info("Data discovery has been finished")
