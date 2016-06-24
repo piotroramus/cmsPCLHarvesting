e14c49181f2aa88102a4db401115c06bec47b1e0
Piotr Oramus <piotr.karol.oramus@cern.ch>
2016-06-24 15:47:43 +0200

    Handling logic of processed multirun

--- python/resultsHandler.py
+++ python/resultsHandler.py
@@ -1,10 +1,37 @@
+import argparse
 import logging
+import sqlalchemy
+
+import model
 import logs.logger as logs
 
 if __name__ == '__main__':
 
+    # TODO: it might be possible to extract similar logic from uprocessedMultirun.py
+
     logs.setup_logging()
     logger = logging.getLogger(__name__)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("multirun_id", help="id of processed multi-run")
+    parser.add_argument("db_path", help="location of database containing multi-run info")
+    args = parser.parse_args()
+
+    multirun_id = args.multirun_id
+    db_path = args.db_path
+
+    engine = sqlalchemy.create_engine('sqlite:///{}'.format(db_path), echo=False)
+    model.Base.metadata.create_all(engine, checkfirst=True)
+    Session = sqlalchemy.orm.sessionmaker(bind=engine)
+    session = Session()
+
+    multirun = session.query(model.Multirun).filter(model.Multirun.id == multirun_id).one()
+    processed_status = session\
+        .query(model.MultirunStatus)\
+        .filter(model.MultirunStatus.status == 'processed_ok')\
+        .one()
+    multirun.status = processed_status
+    session.commit()
 
     logger.info("I will somehow handle the result of the AlCaHarvesting step.")
     logger.info("I should upload DropBox payload.")
