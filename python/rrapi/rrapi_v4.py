3b5e3fb3b95780c76b4c4eb3727af19c1be27c39
Piotr Oramus <piotr.karol.oramus@cern.ch>
2016-07-13 12:52:27 +0200

    Updated Run Registry Api v4

--- python/rrapi/rrapi_v4.py
+++ python/rrapi/rrapi_v4.py
@@ -1,9 +1,8 @@
 import urllib2
 import re
-import simplejson as json
+import json
 import sys
 import xml.dom.minidom as minidom
-
 """
 Python object that enables connection to RestHub API.
 Errors, fixes and suggestions to be sent to project 
@@ -28,6 +27,17 @@ class RhApiPageSizeError(Exception):
         
     def __str__(self):
         return 'Page size (%d) is larger than rows limit (%d) for a single result' % (self.pageSize, self.rowsLimit)
+
+class BadColumnNameError(Exception):
+    
+    def __init__(self, bad_column, columns_list, table_name):
+        self.bad_column = bad_column
+        self.columns_list = columns_list
+        self.table_name = table_name
+    def __str__(self):
+        return 'Column name (%s) does not exist in the table (%s). Try these columns: (%s).'\
+             % (self.bad_column, self.table_name, json.dumps(self.columns_list))
+
 
 
 class RhApi:
@@ -57,23 +67,21 @@ class RhApi:
                 print arg, 
             print
 
-    def get(self, parts, data = None, headers = None, params = None, verbose = False):
+    def get(self, parts, data = None, headers = None, params = None, verbose = False, cols = False):
         """
         General API call (do not use it directly!)
         """
+
+        if type(params) != dict: params = {}
+        if verbose: params["_verbose"] = True
+        if cols: params["_cols"] = True
 
         #
         # Constructing request path
         #
 
         callurl = self.url + "/".join(urllib2.quote(str(p)) for p in parts)
-        if params != None:
-            callurl = callurl + "?" + "&".join(p + "=" + urllib2.quote(str(params[p])) for p in params.keys())
-            if verbose:
-                callurl = callurl + "&_verbose"
-        else:
-            if verbose:
-                callurl = callurl + "?_verbose"
+        callurl = callurl + "?" + "&".join(p + "=" + urllib2.quote(str(params[p])) for p in params.keys())
 
         sdata = None
         if data != None:
@@ -110,6 +118,12 @@ class RhApi:
             else:
                 return rdata
 
+    def info(self, verbose = False):
+        """
+        Get server version information
+        """
+        return self.get(["info"], verbose = verbose)
+    
     def folders(self, verbose = False):
         """
         Get list of folders
@@ -151,7 +165,7 @@ class RhApi:
         """
         return int(self.get(["query", qid, "count"], params = params, verbose = verbose))
 
-    def data(self, qid, params = None, form = 'text/csv', pagesize = None, page = None, verbose = False):
+    def data(self, qid, params = None, form = 'text/csv', pagesize = None, page = None, verbose = False, cols = False):
         """
         Get data rows
         """
@@ -170,7 +184,7 @@ class RhApi:
                 ps.extend(["page", pagesize, page]);
                 
         ps.append("data")
-        return self.get(ps, None, { "Accept": form }, params, verbose = verbose)
+        return self.get(ps, None, { "Accept": form }, params, verbose = verbose, cols = cols)
 
     def csv(self, query, params = None, pagesize = None, page = None, verbose = False):
         """
@@ -186,12 +200,19 @@ class RhApi:
         qid = self.qid(query)
         return self.data(qid, params, 'text/xml', pagesize, page, verbose = verbose)
 
-    def json(self, query, params = None, pagesize = None, page = None, verbose = False):
+    def json(self, query, params = None, pagesize = None, page = None, verbose = False, cols = False):
         """
-        Get rows in JSON format 
+        Get rows in JSON format (array of arrays)
         """
         qid = self.qid(query)
-        return self.data(qid, params, 'application/json', pagesize, page, verbose = verbose)
+        return self.data(qid, params, 'application/json', pagesize, page, verbose = verbose, cols = cols)
+    
+    def json2(self, query, params = None, pagesize = None, page = None, verbose = False, cols = False):
+        """
+        Get rows in JSON2 format (array or objects)
+        """
+        qid = self.qid(query)
+        return self.data(qid, params, 'application/json2', pagesize, page, verbose = verbose, cols = cols)
 
 from optparse import OptionParser
 import pprint
@@ -199,7 +220,7 @@ import pprint
 USAGE = 'usage: %prog [-v] [-u URL] [ FOLDER | FOLDER.TABLE | QUERY ]'
 DEFAULT_URL = "http://vocms00169:2113"
 DEFAULT_FORMAT = "csv"
-FORMATS = [ "csv", "xml", "json" ]
+FORMATS = [ "csv", "xml", "json", "json2" ]
 
 class CLIClient:
     
@@ -209,16 +230,84 @@ class CLIClient:
         self.parser = OptionParser(USAGE)
         self.parser.add_option("-v", "--verbose",  dest = "verbose",  help = "verbose output", action = "store_true", default = False)
         self.parser.add_option("-u", "--url",      dest = "url",      help = "service URL", metavar = "URL", default=DEFAULT_URL)
-        self.parser.add_option("-f", "--format",   dest = "format",   help = "data output format for QUERY data (%s)" % ",".join(FORMATS), metavar = "FORMAT", default=DEFAULT_FORMAT)
+        self.parser.add_option("-f", "--format",   dest = "format",   help = "data output format for QUERY data (%s)" % ",".join(FORMATS), metavar = "FORMAT")
         self.parser.add_option("-c", "--count",    dest = "count",    help = "instead of QUERY data return # of rows", action = "store_true", default = False)
         self.parser.add_option("-s", "--size",     dest = "size",     help = "number of rows per PAGE return for QUERY", metavar = "SIZE", type="int")
         self.parser.add_option("-g", "--page",     dest = "page",     help = "page number to return. Default 1", metavar = "PAGE", default = 1, type="int")
+        self.parser.add_option("-l", "--cols",     dest = "cols",     help = "add column metadata if possible. Default: false", action = "store_true", default = False)
+        self.parser.add_option("-i", "--info",     dest = "info",     help = "print server version information", action = "store_true", default = False)
         self.parser.add_option("-a", "--all",      dest = "all",      help = "force to retrieve ALL data (can take long time)", action = "store_true", default = False)
         self.parser.add_option("-m", "--metadata", dest = "metadata", help = "do not execute query but dump METADATA", action = "store_true", default = False)
         self.parser.add_option("-p",               dest = "param",    help = "parameter for QUERY in form -pNAME=VALUE", metavar = "PARAM", action="append")
 
     def pprint(self, data):
         self.pp.pprint(data)
+
+    def basicSelect(self, arg, api, param, verbose):
+        split_arg = arg.split(".")
+        table_metadata = api.table(split_arg[0], split_arg[1], verbose=verbose)
+        table_name = split_arg[1]
+        # get table names list from meta data
+        column_names_list = []
+        for i in table_metadata["columns"]:
+            column_names_list.append(i["name"])
+        
+        arg = 'select * from ' + arg + ' a'
+        # get and save to list values from p parameters
+        params_length = 0
+        if param is not None:
+            param.sort()
+            
+        if param:
+            param_column_names_list = []
+            param_column_value_list = [] 
+            split_where = []
+            params_length = len(param)
+            # assign param values to lists
+            for i in param:
+                split_where = i.split("=")
+                param_column_names_list.append(split_where[0])
+                param_column_value_list.append(split_where[1])
+            
+            # check if value of first parameter belongs to column names                                
+            for i in param_column_names_list:
+                if i not in column_names_list:
+                    raise BadColumnNameError(i, column_names_list, table_name)
+                
+        if params_length != 0:
+            # build where statements            
+            previous = next = None
+            for index, obj in enumerate(param_column_names_list):
+                if index > 0:
+                    previous = param_column_names_list[index-1]
+                if index < (params_length -1):
+                    next = param_column_names_list[index+1]
+                if params_length == 1:
+                    arg = arg + " where a." + param_column_names_list[index] + " = :" + param_column_names_list[index]                
+                else:
+                    if index == 0:
+                        if param_column_names_list[index] == next:
+                            arg = arg + " where (a." + param_column_names_list[index] + " = :" + param_column_names_list[index]
+                        else:
+                            arg = arg + " where a." + param_column_names_list[index] + " = :" + param_column_names_list[index]
+                    else:
+                        if previous == param_column_names_list[index]:
+                            
+                            if param_column_names_list[index] != next:
+                                arg = arg + " or a." + param_column_names_list[index] + " = :" + param_column_names_list[index] + str(index) + ")"
+                                param[index] = param_column_names_list[index] + str(index) + '=' + param_column_value_list[index]
+                            else:
+                                arg = arg + " or a." + param_column_names_list[index] + " = :" + param_column_names_list[index] + str(index)
+                                param[index] = param_column_names_list[index] + str(index) + '=' + param_column_value_list[index]
+                        else:
+                            if param_column_names_list[index] != next:
+                                arg = arg + " and a." + param_column_names_list[index] + " = :" + param_column_names_list[index]
+                            else:
+                                arg = arg + " and ( a." + param_column_names_list[index] + " = :" + param_column_names_list[index]                  
+                
+                previous = next = None
+                
+        return arg, param
 
     def run(self):
 
@@ -227,6 +316,11 @@ class CLIClient:
             (options, args) = self.parser.parse_args()
 
             api = RhApi(options.url, debug = options.verbose)
+
+            # Info
+            if options.info:
+                self.pprint(api.info(verbose = options.verbose))
+                return 0
 
             # Folders
             if len(args) == 0:
@@ -244,15 +338,22 @@ class CLIClient:
                 self.pprint(api.tables(arg, verbose = options.verbose))
                 return 0
 
+
             # FOLDER.TABLE
-            if re.match("^[a-zA-Z0-9_]+\\.[a-zA-Z0-9_]+$", arg) is not None:
+            if ((re.match("^[a-zA-Z0-9_]+\\.[a-zA-Z0-9_]+$", arg) is not None) and (options.format is None)):
                 parts = arg.split(".")
                 self.pprint(api.table(parts[0], parts[1], verbose = options.verbose))
                 return 0
-
+            
+            # if format is Null, assign format to default format
+            if options.format is None:
+                options.format = DEFAULT_FORMAT
+            
             # QUERY
-            if re.match("^select ", arg, re.IGNORECASE) is not None:
-
+            if re.match("^select ", arg, re.IGNORECASE) is not None or \
+            (re.match("^[a-zA-Z0-9_]+\\.[a-zA-Z0-9_]+$", arg) is not None and (options.format is not None)):
+                if (re.match("^[a-zA-Z0-9_]+\\.[a-zA-Z0-9_]+$", arg) is not None and (options.format is not None)):
+                    arg, options.param = self.basicSelect(arg, api, options.param, options.verbose)                    
                 params = {}
                 if options.param:
                     for ps in options.param:
@@ -313,9 +414,16 @@ class CLIClient:
                                 else:
                                     raise e
 
-                        if options.format == 'json':
+                        if options.format in ['json','json2']:
                             try:
-                                print api.json(arg, params = params, pagesize = options.size, page = options.page, verbose = options.verbose)
+                                if options.format == 'json':
+                                    print api.json(arg, params = params, pagesize = options.size, page = options.page, verbose = options.verbose, cols = options.cols)
+                                    #print_json = api.json(arg, params=params, pagesize=options.size, page=options.page, verbose=options.verbose, cols=options.cols)
+                                    #print (json.dumps(print_json, sort_keys=True, indent=4, separators=(',', ': ')))
+                                else:
+                                    print api.json2(arg, params = params, pagesize = options.size, page = options.page, verbose = options.verbose, cols = options.cols)
+                                    #print_json = api.json(arg, params=params, pagesize=options.size, page=options.page, verbose=options.verbose, cols=options.cols)
+                                    #print (json.dumps(print_json, sort_keys=True, indent=4, separators=(',', ': ')))
                             except RhApiRowLimitError, e:
                                 if options.all:
                                     page = 0
