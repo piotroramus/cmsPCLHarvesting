2bc0192ada4c55f6d3799097dae3a32411fb7b5a
Piotr Oramus <piotr.karol.oramus@cern.ch>
2016-07-20 12:19:26 +0200

    Skeleton for testing of update_runs

--- test/dataDiscovery/discover_test.py
+++ test/dataDiscovery/discover_test.py
@@ -1,10 +1,54 @@
 import os
+import sqlalchemy
 import sys
+import time
 import unittest
+
+from sqlalchemy.ext.declarative import declarative_base
 
 sys.path.insert(0, os.path.abspath('../../python'))
 
 from dataDiscovery import discover
+from t0wmadatasvcApi.t0wmadatasvcApi import Tier0Api
+
+
+class LoggerMock(object):
+    def __init__(self, level):
+        self.available_levels = {
+            "DEBUG": 1,
+            "INFO": 2,
+            "WARNING": 3,
+            "ERROR": 4
+        }
+        if level not in self.available_levels:
+            raise ValueError('Invalid logging level')
+        self.level = self.available_levels[level]
+
+    def debug(self, msg):
+        if self.level <= self.available_levels["DEBUG"]:
+            print msg
+
+    def info(self, msg):
+        if self.level <= self.available_levels["INFO"]:
+            print msg
+
+    def warning(self, msg):
+        if self.level <= self.available_levels["WARNING"]:
+            print msg
+
+    def error(self, msg):
+        if self.level <= self.available_levels["ERROR"]:
+            print msg
+
+
+class T0ApiStreamAlwaysCompleted(Tier0Api):
+    def run_stream_completed(self, run_number):
+        return True
+
+
+class T0ApiStreamNeverCompleted(Tier0Api):
+    def run_stream_completed(self, run_number):
+        return False
 
 
 class DiscoverTest(unittest.TestCase):
@@ -66,6 +110,27 @@ class DiscoverTest(unittest.TestCase):
         expected_result = {'Collisions15', 'Collisions16', 'A', 'B', 'C', 'E', 'ILOVE_CMS'}
         run_class_names = discover.get_run_class_names(imaginary_workflow_run_classes)
         self.assertEqual(run_class_names, expected_result)
+
+    def test_update_runs(self):
+
+        date = time.strftime("%d-%m-%Y_%H.%M.%S")
+        engine = sqlalchemy.create_engine('sqlite:///test-{}.db'.format(date), echo=False)
+        Base = declarative_base()
+        Base.metadata.create_all(engine, checkfirst=True)
+        Session = sqlalchemy.orm.sessionmaker(bind=engine)
+        session = Session()
+
+        logger = LoggerMock('INFO')
+        t0api_stream_completed = T0ApiStreamAlwaysCompleted()
+        t0api_stream_not_completed = T0ApiStreamNeverCompleted()
+
+        config = {"run_stream_timeout": 5}
+        local_runs = []
+        recent_runs = []
+
+        discover.update_runs(logger, session, t0api_stream_completed, config, local_runs, recent_runs)
+
+        # TODO: finish when RRApi server is working since real data is needed
 
 
 if __name__ == '__main__':
