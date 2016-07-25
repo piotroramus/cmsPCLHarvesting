5076b9a8aa6d4c345ae7a8c7ce0c355a96225efe
Piotr Oramus <piotr.karol.oramus@cern.ch>
2016-07-25 15:27:11 +0200

    Created python script responsible for uploading of DQM file and dropbox payload

--- python/runUploads.py
+++ python/runUploads.py
@@ -1,13 +1,23 @@
 import argparse
 import logging
+import os
+import re
 import sqlalchemy
+import subprocess
+import sys
 
 import model
 import logs.logger as logs
 import utils.configReader as configReader
 
-if __name__ == '__main__':
 
+def extract_dataset_parts(dataset):
+    pattern = r'/(?P<primary_dataset>.*)/(?P<era_wf_ver>.*?)/ALCAPROMPT'
+    ds = re.match(pattern, dataset)
+    return ds.group('primary_dataset'), ds.group('era_wf_ver')
+
+
+if __name__ == '__main__':
     logs.setup_logging()
     logger = logging.getLogger(__name__)
 
@@ -29,8 +39,40 @@ if __name__ == '__main__':
 
     multiruns_processed_ok = session \
         .query(model.Multirun) \
-        .filter(model.MultirunState == 'processed_ok') \
+        .filter(model.MultirunState.state == 'processed_ok') \
         .all()
 
     for multirun in multiruns_processed_ok:
-        logger.info("Proceeding with uploads for multirun {}".format(multirun.id))
+        logger.info("Proceeding with DQM file upload for multirun {}".format(multirun.id))
+        logger.info("Determining DQM filename...")
+
+        run_numbers = [run.number for run in multirun.run_numbers]
+        min_run, max_run = min(run_numbers), max(run_numbers)
+
+        primary_dataset, era_wf_ver = extract_dataset_parts(multirun.dataset.dataset)
+
+        dqm_filename = 'DQM_V0001_R000999999__{}__{}-{}-{}__ALCAPROMPT.root' \
+            .format(primary_dataset, era_wf_ver, min_run, max_run)
+
+        # TODO: multirun id could be __different__ when multirun is repeated
+        dqm_file_location = "{}/{}/{}/{}/{}" \
+            .format(config['eos_workspace_path'], multirun.scram_arch, multirun.cmssw, multirun.id, dqm_filename)
+
+        script_path = os.path.dirname(os.path.realpath(__file__))
+        shell_script_path = script_path.replace("/python", "/bin/dqm_upload.sh")
+        cmd = "{} {} {} {} {}".format(shell_script_path, dqm_filename, dqm_file_location, config['dqm_current'],
+                                      config['dqm_upload_host'])
+        result = subprocess.call(cmd, shell=True)
+
+        # this will execute after the subprocess exists
+
+        if result != 0:
+            dqm_failed_state = session \
+                .query(model.MultirunState) \
+                .filter(model.MultirunState.state == 'dqm_upload_failed') \
+                .one()
+            multirun.state = dqm_failed_state
+            session.commit()
+            sys.exit(1)
+
+        # TODO: try to upload payload to dropbox here
