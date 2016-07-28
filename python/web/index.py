4284d7fbdc643b1f6e76c99b9135ef088004fd15
Piotr Oramus <piotr.karol.oramus@cern.ch>
2016-07-28 13:06:02 +0200

    Loading config file in web application

--- python/web/index.py
+++ python/web/index.py
@@ -1,18 +1,36 @@
 import os
 import sys
 
-# in order to import higher level modules
+# in order to import 'higher' level modules
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 import model
+import utils.configReader as configReader
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../test.db'
 
+
+def load_config():
+    config_file = "../config/local.yml"
+    fconfig = os.getenv('FLASK_CONFIG')
+
+    if fconfig:
+        config_file = fconfig
+
+    config = configReader.read(config_file)
+    app.config.update(config)
+
+    # sqlalechmy_db = "sqlite:///{}.db".format(config['db_path'])
+    sqlalechmy_db = "sqlite:///../{}.db".format(config['db_path'])
+    app.config['SQLALCHEMY_DATABASE_URI'] = sqlalechmy_db
+
+
+load_config()
 db = SQLAlchemy(app)
+
 
 def get_data():
     return db.session.query(model.Multirun).all()
@@ -23,9 +41,13 @@ def hello():
     return "Hello World!"
 
 
+@app.route('/test/')
+def test():
+    return "response"
+
+
 @app.route('/display/')
 def display():
-
     result = ""
     for multirun in get_data():
         result = "{}MID: {}\tDATASET: {}\n".format(result, multirun.id, multirun.dataset)
