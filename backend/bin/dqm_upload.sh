31b816fbc4d4a907239d2a3a05fae886de516441
Piotr Oramus <piotr.karol.oramus@cern.ch>
2016-08-30 16:04:34 +0200

    Fixed DQM file uploading by providing exact paths

--- backend/bin/dqm_upload.sh
+++ backend/bin/dqm_upload.sh
@@ -9,6 +9,9 @@ DQM_FILENAME="$1"
 DQM_FILE_LOCATION="$2"
 DQM_UPLOAD_HOST="$4"
 MULTIRUN_ID="$5" #TODO: fix this after it finally works
+PYTHON_DIR="$6"
+
+echo "PYTHON DIR: $PYTHON_DIR"
 
 echo -e "\n Starting to perfrom DQM upload"
 echo "Multi-run ID: ${MULTIRUN_ID}"
@@ -26,11 +29,11 @@ EOS_RC=$?
 if [ ${EOS_RC} -ne 0 ]; then
     echo "ERROR: Trying to copy $DQM_FILE_LOCATION from eos resulted in error"
     echo "EOS command returned $EOS_RC"
-    exit $EOS_RC
+    exit ${EOS_RC}
 fi
 
 echo "Uploading $DQM_FILENAME to DQM"
-python ../visDQMUpload $DQM_UPLOAD_HOST $DQM_FILENAME
+python ${PYTHON_DIR}/visDQMUpload.py $DQM_UPLOAD_HOST $DQM_FILENAME
 UPLOAD_RC=$?
 
 echo "DQM exit code: $UPLOAD_RC"
