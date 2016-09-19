7e7ff1b8a9fef3ba0ff84195b412e7f01321a333
Piotr Karol Oramus <piotr.karol.oramus@cern.ch>
2016-09-19 10:58:10 +0200

    Passing grid pw to setup through script argument rather than file

--- backend/bin/setup.sh
+++ backend/bin/setup.sh
@@ -1,21 +1,15 @@
 #!/usr/bin/env bash
 
+if [ "$#" -ne 1 ]; then
+    echo "Script usage: source ${BASH_SOURCE} GRID_CERT_PASS_PHRASE"
+    exit 1
+fi
+
+GRID_CERT_PASS_PHRASE="$1"
 
 echo "Generating new proxy certificate..."
 
-CREDENTIALS_FILE="$HOME/.credentials"
-if [ ! -f $CREDENTIALS_FILE ]; then
-    echo "Error: $CREDENTIALS_FILE file is missing!"
-    exit 1
-fi
-
-source $CREDENTIALS_FILE
-if [ -z ${GRID_PASS_PHRASE+x} ]; then
-    echo "Error: GRID_PASS_PHRASE not set in $CREDENTIALS_FILE!"
-    exit 1
-fi
-
-echo $GRID_PASS_PHRASE | voms-proxy-init -rfc -voms cms -pwstdin
+echo $GRID_CERT_PASS_PHRASE | voms-proxy-init -rfc -voms cms -pwstdin
 rc=$?
 if [ $rc -eq 3 ]; then
     echo "Error: Wrong passphrase for GRID certificate!"
