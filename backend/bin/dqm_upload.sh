452cb24fcda8d28a15914b0fcecc8c47e01ec55d
Piotr Karol Oramus <piotr.karol.oramus@cern.ch>
2016-11-29 10:33:32 +0100

    Using common functions in DQM uplaod

--- backend/bin/dqm_upload.sh
+++ backend/bin/dqm_upload.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-function eos() {
-   /afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select "$@"
-}
-
+# source common.sh which contains eos function definition
+BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source ${BIN_DIR}/common.sh
 
 DQM_FILENAME="$1"
 DQM_FILE_LOCATION="$2"
@@ -16,29 +15,29 @@ echo "Multi-run ID: ${MULTIRUN_ID}"
 echo -e "\n"
 
 TMP_DIR=upload_tmp
-echo "Creating temporary directory $TMP_DIR"
+echo "Creating temporary directory ${TMP_DIR}"
 mkdir ${TMP_DIR}
 cd ${TMP_DIR}
 
-echo "Copying $DQM_FILE_LOCATION from EOS"
-eos cp --checksum --preserve $DQM_FILE_LOCATION $DQM_FILENAME
+echo "Copying ${DQM_FILE_LOCATION} from EOS"
+eos cp --checksum --preserve ${DQM_FILE_LOCATION} ${DQM_FILENAME}
 EOS_RC=$?
 
 if [ ${EOS_RC} -ne 0 ]; then
-    echo "ERROR: Trying to copy $DQM_FILE_LOCATION from eos resulted in error"
-    echo "EOS command returned $EOS_RC"
+    echo "ERROR: Trying to copy ${DQM_FILE_LOCATION} from eos resulted in error"
+    echo "EOS command returned ${EOS_RC}"
     exit ${EOS_RC}
 fi
 
-echo -e "\nUploading $DQM_FILENAME to DQM"
+echo -e "\nUploading ${DQM_FILENAME} to DQM"
 echo -e "Target GUI: ${DQM_UPLOAD_HOST} \n"
 
-python ${PYTHON_DIR}/visDQMUpload.py $DQM_UPLOAD_HOST $DQM_FILENAME
+python ${PYTHON_DIR}/visDQMUpload.py ${DQM_UPLOAD_HOST} ${DQM_FILENAME}
 UPLOAD_RC=$?
 
-echo "DQM exit code: $UPLOAD_RC"
+echo "DQM exit code: ${UPLOAD_RC}"
 
-echo "Removing temporary directory $TMP_DIR"
+echo "Removing temporary directory ${TMP_DIR}"
 cd ..
 rm -r ${TMP_DIR}
 
